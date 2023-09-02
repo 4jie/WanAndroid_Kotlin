f6498eac52026ef3f12952fc6b9b480dc2b8519b
4jie <2485490573@qq.com>
2023-09-03 00:22:02 +0800

    feat(/main/res)：添加一些资源文件

---
+++ lib_common/src/main/java/com/caisijie/common/constant/IntentKey.kt
@@ -0,0 +1,16 @@
+package com.caisijie.common.constant
+
+/**
+ * @author caisijie
+ * @date 2023/9/2
+ * @description 统一封装Intent所需要的key
+ */
+const val KEY_ID = "id"
+const val KEY_LIST = "list"
+const val KEY_DATA = "data"
+const val KEY_TYPE = "type"
+const val KEY_INDEX = "index"
+const val KEY_URL = "url"
+const val KEY_TITLE = "title"
+const val KEY_POSITION = "position"
+const val KEY_VIDEO_PLAY_LIST = "video_play_list"
--- mod_main/src/main/java/com/caisijie/main/MainActivity.kt
+++ mod_main/src/main/java/com/caisijie/main/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.caisijie.main
 
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.navigation.NavController
+import com.caisijie.common.constant.KEY_INDEX
 import com.caisijie.framework.base.BaseDataBindActivity
 import com.caisijie.main.databinding.ActivityMainBinding
 
 @Route
 class MainActivity : BaseDataBindActivity<ActivityMainBinding>() {
+    private lateinit var navController:NavController
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    companion object{
+        fun start(context: Context, index:Int=0){
+            val intent=Intent(context,MainActivity::class.java)
+            intent.putExtra(KEY_INDEX,index)
+            context.startActivity(intent)
+        }
     }
 
     override fun initView(savedInstanceState: Bundle?) {
-
+        setTheme(R.style.AppTheme)
     }
 }
